f6761caf09136434178741607b70bc9e5f3d54a2
Ross Godfrey <ross.m.godfrey@gmail.com>
2022-05-26 15:12:08 +0200

    List logic done

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,29 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    # For now, try it with `list` to see if it works. If not, change to `@list`.
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
