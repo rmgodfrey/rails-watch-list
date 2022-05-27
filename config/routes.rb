3156b0bb1ffb695a7ac47943532b1c0f268a16c9
Ross Godfrey <ross.m.godfrey@gmail.com>
2022-05-27 12:39:58 +0200

    Added destroy actions to list and bookmark

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "lists#index"
-  resources :lists, only: %I[index show new create] do
+  resources :lists, only: %I[index show new create destroy] do
     resources :bookmarks, only: %I[new create]
   end
   resources :bookmarks, only: %I[destroy]
